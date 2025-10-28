13a9ef3c1d15d2a853313bba1792fe4c92ea1e9d
Aya Mydani <ayamidani98@gmail.com>
2025-10-28 23:02:43 +0300

    Initial commit - ASP.NET Core MVC Expense Tracker project

---
+++ Models/Transaction.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ExpenseTracker.Models;
+
+public class Transaction
+{
+    [Key]
+    public int TransactionId { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } // navigation property
+    public int Amount { get; set; }
+
+    [Column(TypeName = "nvarchar(50)")]
+    public string? Note { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
+}
