54cda041dd6b5eb6d6d8004fe007529488657ce5
Aya Mydani <ayamidani98@gmail.com>
2026-01-02 21:23:25 +0300

    data added

--- obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a9dc69dcb7a464ed4002571c9b0469bf2f55592")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7921bbe835edce79184615389436a2f00acaa65")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
